48b45686e29ae0a35184b2d6af886fa2bfe4935d
Keks <keks@htmlacademy.ru>
2023-06-27 00:47:50 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/constans.js
@@ -0,0 +1,7 @@
+export const MAX_GENERATED_OBJECT_COUNT = 25;
+export const MIN_PHOTO_COUNT = 1;
+export const MAX_PHOTO_COUNT = 25;
+export const MIN_LIKE_SCORE = 15;
+export const MAX_LIKE_SCORE = 200;
+export const MAX_AVATAR_COUNT = 1;
+export const MIN_AVATAR_COUNT = 6;
