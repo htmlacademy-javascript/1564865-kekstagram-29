976dd6d218906150a4b0df4f142087603a9e0d1f
Keks <keks@htmlacademy.ru>
2023-07-15 22:26:14 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,4 @@
+import { renderGallery } from './gallery.js';
+import { photosArray } from './generation-photo.js';
+import './form.js';
+renderGallery(photosArray);
---
+++ 12/js/util.js
@@ -0,0 +1,39 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createGeneratorInRange = (firstIndex, lastIndex) => {
+  const previousValues = [];
+  return () => {
+    let currentValue = firstIndex;
+
+    if (previousValues.length >= lastIndex) {
+      return null;
+    }
+
+    while (previousValues.includes(currentValue)) {
+      currentValue++;
+    }
+
+    previousValues.push(currentValue);
+    return currentValue;
+
+  };
+};
+const generatePhotoId = createGeneratorInRange(1, 100);
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+function handleKeyDown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+}
+
+export { getRandomArrayElement, getRandomInteger, isEscapeKey, generatePhotoId, handleKeyDown };
